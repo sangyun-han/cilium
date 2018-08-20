d59d02754a80c97a5eb6a9a11cc2ab29332784b4
André Martins <andre@cilium.io>
2018-08-20 21:30:09 +0200

    validate OptionMap at the API layer for daemon and endpoint options
    This moves the Options validation closer to the API layer to avoid
    holding any mutexes while the validation is behing verified.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1483,11 +1483,18 @@ func (h *patchConfig) Handle(params PatchConfigParams) middleware.Responder {
 
 	d := h.daemon
 
+	cfgSpec := params.Configuration
+
+	om, err := option.DaemonOptionLibrary.ValidateConfigurationMap(cfgSpec.Options)
+	if err != nil {
+		msg := fmt.Errorf("Invalid configuration option %s", err)
+		return api.Error(PatchConfigFailureCode, msg)
+	}
+
 	// Serialize configuration updates to the daemon.
 	option.Config.ConfigPatchMutex.Lock()
 	defer option.Config.ConfigPatchMutex.Unlock()
 
-	cfgSpec := params.Configuration
 	nmArgs := d.nodeMonitor.GetArgs()
 	if numPagesEntry, ok := cfgSpec.Options["MonitorNumPages"]; ok && nmArgs[0] != numPagesEntry {
 		if len(nmArgs) == 0 || nmArgs[0] != numPagesEntry {
@@ -1529,7 +1536,7 @@ func (h *patchConfig) Handle(params PatchConfigParams) middleware.Responder {
 		log.Debug("finished configuring PolicyEnforcement for daemon")
 	}
 
-	changes += option.Config.Opts.ApplyValidated(cfgSpec.Options, changedOption, d)
+	changes += option.Config.Opts.ApplyValidated(om, changedOption, d)
 
 	log.WithField("count", changes).Debug("Applied changes to daemon's configuration")
 
